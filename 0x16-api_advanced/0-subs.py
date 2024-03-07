f59118a13445a012775f2e7bcba7eefb9ab66917
elyton250 <nelysemathieu@gmail.com>
2024-03-07 17:32:02 +0200

    last task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,7 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    
+
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {"User-Agent": "My-User-Agent"}
     response = requests.get(url, headers=headers, allow_redirects=False)
